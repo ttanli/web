512ea8153c100f5754673832947805fc0ec3e05c
medmin <erichui329@gmail.com>
2019-06-15 17:50:40 -0700

    init, setup react without create-react-app

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'main.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                loader:'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title:'天天安利日报',
+            template: './public/index.html'
+        })
+    ],
+    devServer: {
+        open: true,
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 8081
+    }
+}
