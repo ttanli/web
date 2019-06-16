6e8780829eb047834f56b5309760a8936d22cef9
medmin <erichui329@gmail.com>
2019-06-15 18:29:00 -0700

    optimize webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,10 @@ module.exports = {
         rules:[
             {
                 test: /\.js$/,
-                loader:'babel-loader'
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
             }
         ]
     },
