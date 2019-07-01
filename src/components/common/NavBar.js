12e21ab81d0a52c53be1c6b2557c8a93b2c9e554
medmin <erichui329@gmail.com>
2019-06-30 19:34:27 -0700

    add search bar and user icon

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import logo from "@/assets/logo.png";
-import { AppBar, Toolbar, Grid, Avatar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Grid, Avatar, Typography, TextField, Menu, MenuItem } from "@material-ui/core";
+import Icon from "@material-ui/core/Icon";
 
 class NavBar extends Component {
+  handleClickAccountIcon = () => {
+    console.log("click");
+  };
+
   render() {
     return (
-      <AppBar>
-        <Toolbar variant="dense">
-          <Grid container alignContent="flex-start" alignItems="center" spacing={1}>
+      <AppBar color="inherit">
+        <Toolbar variant="regular">
+          <Grid container alignItems="center" spacing={1}>
             <Grid item>
               <Avatar alt="logo" src={logo} />
             </Grid>
@@ -15,6 +20,14 @@ class NavBar extends Component {
               <Typography variant="subtitle1" noWrap>
                 天天安利 • 日报
               </Typography>
+            </Grid>
+            <Grid item>
+              <TextField label="Search..." margin="dense" variant="outlined" />
+            </Grid>
+            <Grid item onClick={this.handleClickAccountIcon}>
+              <Grid container justify="space-around">
+                <Icon>account_circle</Icon>
+              </Grid>
             </Grid>
           </Grid>
         </Toolbar>
